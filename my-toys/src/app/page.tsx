b76f8797addd515e28ec903086b7a19cbf6a66df
suzuki <yusuke.works2450@gmail.com>
2025-01-16 23:44:44 +0900

    修正

---
+++ my-toys/src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import TodoList from "./components/TodoList";
+import AddTask from "./components/AddTask";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
+      <h1 className="text-4xl font-bold text-gray-700 -mt-32">Top Pages</h1>
+      <div className="w-full max-w-xl mt-5">
+        <div className="w-full px-8 py-6 bg-white shadow-md rounded-lg">
+          <AddTask />
+          <TodoList/>
+        </div>
+      </div>
+    </main>
+  );
+}
